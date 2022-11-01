eae53cd4548303c0dc80166b0a879a59262ab059
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-11-01 15:54:25 -0300

    Removed innescesary code

--- lib/src/screens/maps_screen.dart
+++ lib/src/screens/maps_screen.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_qr_scanner/src/widgets/custom_dismissible.dart';
-import 'package:flutter_qr_scanner/src/providers/scan_list_provider.dart';
 
 class MapsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider =
-        Provider.of<ScanListProvider>(context, listen: false);
-
-    final scanList = scanListProvider.scanList;
-
     return CustomDismissible(type: "geo");
   }
 }
